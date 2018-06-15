244cb1d206556190e1577791acd174e0c00352fc
blockbyblock <ybingcheng@gmail.com>
2018-06-16 00:16:40 +0800

    minor correction

--- call_button_cpp/call_button_np.cpp
+++ call_button_cpp/call_button_np.cpp
@@ -18,24 +18,24 @@ public:
             [this]() -> void {
                 auto msg = rmf_msgs::msg::CallButtonState();
                 // Button
-                btn_state = digitalRead(btn_pin);
-                msg.pressed = btn_state;
-                if(btn_state == 1)
+                btn_state = digitalRead(btn_pin);   // Return integer
+                msg.pressed = (btn_state > 0)? true : false;
+                if(msg.pressed == true)
                 {
-                    msg = 1;
                     digitalWrite(led_pin, HIGH);
                 }
                 else
                 {
-                    msg = 0;
                     digitalWrite(led_pin, LOW);
                 }
-                std::cout << "Reading from button: " << msg << std::endl;     
-                RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", msg.pressed)
+                std::cout << "Reading from button: " << msg.pressed << std::endl;     
+                RCLCPP_INFO(this->get_logger(), "ROS2 Pub: '%s'", msg.pressed)
                 this->publisher_->publish(msg);
             };
         timer_ = this->create_wall_timer(1000ms, timer_callback);
     }
+    // Reset call button function
+    // Ping function
 
 private:
     rclcpp::TimerBase::SharedPtr timer_;
