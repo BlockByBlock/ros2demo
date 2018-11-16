9000d25e34d8a0a6307c2a4bc4eb11f3eead3403
blockbyblock <ybingcheng@gmail.com>
2018-11-16 11:22:17 +0800

    Working yaml based waypoints

--- pathplanner/plannerclient.py
+++ pathplanner/plannerclient.py
@@ -9,57 +9,86 @@ todo:
 """
 import rospy
 import actionlib
+import yaml
+import sys
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 
 class PlannerClient(object):
     def __init__(self):
+        self.path = list()
         self.client = actionlib.SimpleActionClient(
             'move_base', MoveBaseAction)
+
+    def start(self):
         self.client.wait_for_server()
 
-    def go_goal(self, goal):
-        """
-        Prepare a coordinate, send to robot, and wait.
-        """
-        # Send goal and wait for result
-        self.client.send_goal(goal)
-        self.client.wait_for_result()
-        if not self.client.wait_for_result():
-            rospy.logerr('Action server not available!')
-            rospy.signal_shutdown('Action server not available!')
-        else:
-            rospy.loginfo('HURRAY Goal completed!')
+    def load_config_file(self, filename):
+        try:
+            f = open(filename, 'r')
+            config = yaml.safe_load(f)
+        except yaml.YAMLError as ex:
+            print(ex)
+            return False
 
-    def prepare_goal(self, coordx, coordy, oriz, oriw):
+        f.close()
+
+        self.path = config['journey']['waypoints']
+
+        print ("Your itinerary in position(x, y) & orientation(z, w):")
+        for step in range(len(self.path)):
+            print ("Step {}: {}").format(step+1, self.path[step])
+
+        return True
+
+    def goal_manager(self):
         """
-        Prepare goal.
+        Prepare goal. Send Goal.
         """
-        # Create goal object
-        goal = MoveBaseGoal()
-        goal.target_pose.header.frame_id = 'map'
-        # goal.target_pose.header.frame_id = 'base_link'
-        goal.target_pose.header.stamp = rospy.Time.now()
-        # Goal coordinates
-        goal.target_pose.pose.position.x = coordx
-        goal.target_pose.pose.position.y = coordy
-        goal.target_pose.pose.orientation.z = oriz
-        goal.target_pose.pose.orientation.w = oriw
-        rospy.loginfo('Set goal (x,y,z,w): %s, %s, %s, %s' % (
-                goal.target_pose.pose.position.x,
-                goal.target_pose.pose.position.y,
-                goal.target_pose.pose.orientation.z,
-                goal.target_pose.pose.orientation.w))
-        return goal
+        for step in range(len(self.path)):
+            # Create goal object
+            goal = MoveBaseGoal()
+            goal.target_pose.header.frame_id = 'map'
+            # goal.target_pose.header.frame_id = 'base_link'
+            goal.target_pose.header.stamp = rospy.Time.now()
+            # Goal coordinates
+            goal.target_pose.pose.position.x = self.path[step][0]
+            goal.target_pose.pose.position.y = self.path[step][1]
+            goal.target_pose.pose.orientation.z = self.path[step][2]
+            goal.target_pose.pose.orientation.w = self.path[step][3]
+            rospy.loginfo('Set goal (x,y,z,w): %s, %s, %s, %s' % (
+                    goal.target_pose.pose.position.x,
+                    goal.target_pose.pose.position.y,
+                    goal.target_pose.pose.orientation.z,
+                    goal.target_pose.pose.orientation.w))
+
+            # Send goal and wait for result
+            self.client.send_goal(goal)
+            self.client.wait_for_result()
+            if not self.client.wait_for_result():
+                rospy.logerr('Action server not available!')
+                rospy.signal_shutdown('Action server not available!')
+            else:
+                print('STATUS: step{} completed!').format(step+1)
+                rospy.loginfo('HURRAY goal completed!')
 
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("usage:   plannerclient.py CONFIG.yaml")
+        print("example: plannerclient.py config/journey.yaml")
+        sys.exit(1)
+
     try:
         rospy.init_node('pathclient')
         pc = PlannerClient()
 
-        goalone = pc.prepare_goal(3.65, 5.03, 0.60, 0.80)
-        pc.go_goal(goalone)
+        if not pc.load_config_file(sys.argv[1]):
+            print("INVALID journey configuration file!")
+            sys.exit(1)
+
+        pc.start()
+        pc.goal_manager()
 
     except rospy.ROSInterruptException:
         rospy.loginfo("Interrupt")
